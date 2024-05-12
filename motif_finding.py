27920509eed21d98413c1724689ead503b1e2f42
Alaa Ashraf <91037437+sottohy@users.noreply.github.com>
2024-05-12 19:54:18 +0300

    Update motif_finding.py

--- motif_finding.py
+++ motif_finding.py
@@ -1,3 +1,7 @@
+import time
+import random
+import string
+
 def brute_force_motif_search(dna, t, n, l):
     bestScore = 0
     bestMotif = None
@@ -12,7 +16,7 @@ def brute_force_motif_search(dna, t, n, l):
             current_sequence = dna[i]
             start = s[i]  # Start index
             current_motifs.append(current_sequence[start:start + l])
-        
+
         # profile matrix
         for j in range(l):
             counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
@@ -22,7 +26,6 @@ def brute_force_motif_search(dna, t, n, l):
                 profile_matrix[nucleotide][j] = counts[nucleotide]
 
         # consensus
-        max_numbers = []
         consensus = []
 
         # Iterate over each column index
@@ -31,7 +34,6 @@ def brute_force_motif_search(dna, t, n, l):
             # Get the maximum number and corresponding nucleotide in the current column
             max_in_column, nucleotide = max((profile_matrix[nucleotide][i], nucleotide) for nucleotide in profile_matrix)
             score += max_in_column
-            max_numbers.append(max_in_column)
             consensus.append(nucleotide)
 
         # construct consensus string with correct case
@@ -54,15 +56,82 @@ def brute_force_motif_search(dna, t, n, l):
     print("Motif:", bestMotif)
     print("Score:", bestScore)
 
-# Example usage
-dna_sequences = [
-    "cctgatagacgctatctggctatccaGgtacTtaggtcctctgtgcgaatctatgcgtttccaaccat",
-    "agtactggtgtacatttgatCcAtacgtacaccggcaacctgaaacaaacgctcagaaccagaagtgc",
-    "aaacgtTAgtgcaccctctttcttcgtggctctggccaacgagggctgatgtataagacgaaaatttt",
-    "agcctccgatgtaagtcatagctgtaactattacctgccacccctattacatcttacgtCcAtataca",
-    "ctgttatacaacgcgtcatggcggggtatgcgttttggtcgtcgtacgctcgatcgttaCcgtacgGc"
-]
-sequences = 5
-nucleotides = 68
-pattern_length = 8
-brute_force_motif_search(dna_sequences, sequences, nucleotides, pattern_length)
+
+def hamming_dist(word, seq):
+    minDist = float('inf')
+
+    for i in range(len(seq) - len(word) + 1):  #iterates over seq as long as word can fit in, if word len=4 and seq len=9 , then iterates 6 times
+        pattern = seq[i:i+len(word)]   #get the pattern from the seq with the same length as word
+        mismatch = 0
+
+        for j in range(len(word)):
+            if word[j] != pattern[j]:  #compare
+                mismatch += 1   #if not equal increment mismatch
+
+        minDist = min(minDist, mismatch)
+    return minDist
+
+
+def MedianStringSearch(sequences, num_sequences, sequence_length, motif_length):
+    best_motif = None
+    best_distance = float('inf')
+
+    for i in range(sequence_length - motif_length + 1):  #iterate on all possible sequences of the same motif lenghts
+        motif = sequences[0][i:i+motif_length]  #extract 1st seq starting from index i to motif length (substring of length motif)
+
+        total_distance = sum(hamming_dist(motif, seq) for seq in sequences)   #calc distance from cuurent motif and each seq in the list
+
+         #update dist and best motif found
+        if total_distance < best_distance:
+            best_distance = total_distance
+            best_motif = motif
+
+    print("Motif:", best_motif)
+    print("Hamming Distance:", best_distance)
+
+
+
+print("Motif Finding Problem Solver\n")
+print("Choose input method: \n1. Read from file (rawDNA.txt) \n2. Generate random sequences \n")
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    print("\nReading input from file...")
+
+    file_path = 'rawDNA.txt'
+    file = open(file_path, 'r')
+    file_lines = file.readlines()
+
+    # Remove \n
+    file_lines = [line.strip() for line in file_lines]
+
+    # Reading t, n, l from the first line
+    sequences, nucleotides, pattern_length = file_lines[0].split()
+    sequences = int(sequences)
+    nucleotides = int(nucleotides)
+    pattern_length = int(pattern_length)
+
+    # Remove the first line
+    DNA = file_lines[1:]
+
+elif choice == 2:
+    pattern_length = int(input("Enter the length of the pattern to be found (L): "))
+    sequences = int(input("Enter the number of sequences (t): "))
+    nucleotides = int(input("Enter the length of each sequence (n): "))
+    print("\nGenerating random sequences...")
+    time.sleep(1)
+
+    DNA = []
+    for i in range(sequences):
+      characters = 'ACGT'
+      random_string = ''.join(random.choice(characters) for _ in range(nucleotides))
+      print("Sequence", i+1, ":", random_string)
+      DNA.append(random_string)
+
+
+print("\nBrute Force Algorithm: ")
+brute_force_motif_search(DNA, sequences, nucleotides, pattern_length)
+print("\nMedian String Algorithm: ")
+MedianStringSearch(DNA, sequences, nucleotides, pattern_length)
+
